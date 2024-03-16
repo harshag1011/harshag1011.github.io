989694e01204b4bc761e5b2e984bee423d7f4abc
Harsh Agrawal <harshag1011@gmail.com>
2024-03-16 23:52:54 +0530

    personalized welcome message using js and web storage api

--- first-website/scripts/main.js
+++ first-website/scripts/main.js
@@ -8,3 +8,29 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
 };
+
+
+//change the page title to a personalized welcome message when the user first visits the site. This welcome message will persist.
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+      } else {
+        localStorage.setItem("name", myName);   // web storage api
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+      }
+  }
+
+if (!localStorage.getItem("name")) {
+setUserName();
+} else {
+const storedName = localStorage.getItem("name");
+myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+setUserName();
+};
